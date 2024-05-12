ad3453ef50e46a5d439437fd71f5c5806d0c0983
lizwachira <lizwanguwachira@gmail.com>
2024-05-12 21:53:17 +0300

    feat: dark/light mode check, test passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
+  const [items, setItems] = useState(itemData);
   function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
   return (
+   
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      <Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick} />
+      <ShoppingList items={items} />
     </div>
   );
 }
